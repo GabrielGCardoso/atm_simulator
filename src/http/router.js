f20830461ef015493289fb89ee11ad486a123ef6
GabrielGCardoso <gabrielguimac@gmail.com>
2020-12-16 23:38:32 -0300

    forgot route, adding test feature

--- src/http/router.js
+++ src/http/router.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/client', require('src/http/presentation/routes/clientRoutes'));
+app.use('/user', require('src/http/presentation/routes/userRoutes'));
 
 app.all('*', async (req, res) => {
     res.status(404).send('route not found');
---
+++ test/feature/user/userCreate.spec.js
@@ -0,0 +1,26 @@
+const { describe, it } = require('mocha');
+const { expect, spy } = require('chai');
+const request = require('test/support/request');
+
+describe('API :: POST user/create', () => {
+    context('creating user with success', async () => {
+        let payload;
+        beforeEach(async () => {
+            payload = {
+                name: 'gabriel',
+                cpf: '010.011.012-13',
+                birthday_date: '10/10/2001',
+            };
+        });
+
+        it('Create user - with all fields', async () => {
+            const { body } = await request()
+                .post('/user/create')
+                .send(payload)
+                .expect(201);
+
+            expect(body.id).to.be.exist();
+            expect(body.name).to.be.eql('gabriel');
+        });
+    });
+});
