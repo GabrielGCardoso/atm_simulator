7734c7e367c1269902282f41ee62e902e1a48ae8
GabrielGCardoso <gabrielguimac@gmail.com>
2020-12-21 19:29:28 -0300

    adding debit & balance service

--- src/app/services/transactionService/index.js
+++ src/app/services/transactionService/index.js
@@ -5,8 +5,16 @@ class TransactionService {
         this.repository = new TransactionRepository();
     }
 
+    async debit(account_id, amount) {
+        return this.repository.create({ account_id, amount, type: 2 });
+    }
+
     async create(data) {
         return this.repository.create(data);
+    }
+
+    async getBalance(account_id) {
+        return this.repository.getBalance(account_id);
     }
 }
 
