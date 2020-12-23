7db337f052982e64e4b798a493337e87f6045a31
GabrielGCardoso <gabrielguimac@gmail.com>
2020-12-23 00:55:19 -0300

    adding validation critical zone to debit

--- src/app/services/transactionService/index.js
+++ src/app/services/transactionService/index.js
@@ -1,12 +1,24 @@
 const TransactionRepository = require('src/infra/repository/TransactionRepository');
+const StateMachineService = require('src/app/services/StateMachineService');
 
 class TransactionService {
     constructor() {
         this.repository = new TransactionRepository();
+        this.stateMachine = new StateMachineService();
     }
 
     async debit(account_id, amount) {
-        return this.repository.create({ account_id, amount, type: 2 });
+        if (await this.stateMachine.isInCriticalZoneAccount(account_id))
+            throw Exception.business(
+                'Account in use, if is not you call to bank to report this activity'
+            );
+
+        return this.repository
+            .create({ account_id, amount, type: 2 })
+            .then((result) => {
+                this.stateMachine.freeCriticalZoneAccount(account_id);
+                return result;
+            });
     }
 
     async create(data) {
